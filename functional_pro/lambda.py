604a35a117e5529e27bd68c563a074b74882c006
fazza-tech <ebranfazza@gmail.com>
2024-02-27 22:18:39 +0530

    lambda function

--- functional_pro/lambda.py
+++ functional_pro/lambda.py
@@ -9,3 +9,16 @@ def f(person):
     return person['name']
 peoples.sort(key=f)
 print(peoples)
+
+#we can make our code more readable by using a lambda function:
+
+peoples = [
+    {'name' : "Lijith", 'house': 'Assaripadi' },
+    {'name' : 'Nidhin', 'house': 'Pazhoor'},
+    {'name':'Anusree' ,'house':'makutty'},
+    {'name':'Fazza' ,'house':'Chalakudy'}
+]
+
+
+peoples.sort(key=lambda person:person['name'])
+print(peoples)
